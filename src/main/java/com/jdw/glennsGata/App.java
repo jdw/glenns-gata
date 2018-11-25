91d1bc4191bcf560b08adb5f656e84cfbc8f06e1
Johannes Wirde <johannes.wirde@gmail.com>
2018-11-25 19:44:07 +0100

    Formatting

--- src/main/java/com/jdw/glennsGata/App.java
+++ src/main/java/com/jdw/glennsGata/App.java
@@ -1,48 +1,66 @@
 package com.jdw.glennsGata;
 
-import java.util.Queue;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 /**
  * Hello world!
  *
  */
-public class App {/*
-	private static boolean solve(Board b) {
-		Set<Integer> positions = b.getRemainigPositions();
-		Set<Character> digits = b.getRemainigDigits(); 
+public class App {
+	static private List<Board> solved;
+	static private List<Board> tests;
+	
+	private static void solve(Board b) {
+		Stack<Integer> positions = b.getRemainigPositions();
+		Stack<Character> digits = b.getRemainigDigits(); 
 		
 		if (positions.size() > 1) {
-			Integer pos = (Integer) positions.toArray()[0];
-			Character digit = (Character) digits.toArray()[0];
-			
-			b.setDigitAt(pos, digit);
-			
-			return solve(b);
+			Integer pos = positions.pop();
+			while (!digits.isEmpty()) {
+				Character digit = digits.pop();
+				b.setDigitAt(pos, digit);
+				solve(new Board(b));
+				b.removeDigitAt(pos);
+			}
 		}
 		else if (positions.size() == 1) {
-			Integer pos = (Integer) positions.toArray()[0];
-			Character digit = (Character) digits.toArray()[0];
-			
-			return solve(b);
+			Integer pos = positions.pop();
+			Character digit = digits.pop();
+				
+			b.setDigitAt(pos, digit);
+				
+			solve(new Board(b));
 		}
-		
-		return b.testBoard();
+		else {
+			tests.add(b);
+			//System.out.println(b);
+
+			if (b.testBoard()) {
+				solved.add(new Board(b));
+				
+			}
+		}
 	}
 	
 	
 	public static void solve() {
-		Board b = new Board();
+		solve(new Board());
 		
-		solve(b);
-		
-		System.out.println(b);
+		if (solved.isEmpty()) System.out.println("No solution found");
+		else {
+			System.out.println("Tested " + tests.size()+ " boards, found "+ solved.size() + " solutions:");
+			for (Board b: solved) {
+				System.out.println(b);
+			}
+		}
 	}
-	*/
 	
-    public static void main( String[] args )
-    {
-    	//solve();
-        
+	
+    public static void main( String[] args ) {
+    	App.solved  = new ArrayList<Board>();
+    	App.tests  = new ArrayList<Board>();
+    	solve();
     }
 }
